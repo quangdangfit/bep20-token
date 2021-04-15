3144faeda1286ef1eb61bd7c9499feb0edc43a02
Quang Dang <quangdangfit@gmail.com>
2021-04-15 11:00:59 +0700

    use openzeppelin-solidity library

--- contracts/BEP20.sol
+++ contracts/BEP20.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.5;
 
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 import "./IBEP20.sol";
 import "./Context.sol";
 import "./Ownable.sol";
