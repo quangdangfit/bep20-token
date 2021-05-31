5bc67cc73144047a0b71ca6c28cd72f350da851c
cnexodus <79371593+cnexodus@users.noreply.github.com>
2021-05-31 07:40:55 -0500

    Update Token.sol

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -3,10 +3,10 @@ pragma solidity ^0.5;
 import "./BEP20.sol";
 
 contract Token is BEP20 {
-    string constant public tokenName = "Token";
-    string constant public tokenSymbol = "TOK";
+    string constant public tokenName = "cnExodus";
+    string constant public tokenSymbol = "CNEXV";
     uint8 constant public tokenDecimals = 18;
-    uint256 constant public cap = 2000000000 * (10 ** 18);
+    uint256 constant public cap = 500000000 * (10 ** 18);
 
     constructor() public BEP20(tokenName, tokenSymbol, tokenDecimals) {
         owner = msg.sender;
